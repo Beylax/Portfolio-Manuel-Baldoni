8c62d3596a03187b16ad0a89d1ec1953ca9cb859
beylax <manuel.baldoni04@gmail.com>
2022-08-18 23:00:56 +0200

    Modifiche responsive

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,13 +12,13 @@ class Home extends React.Component {
             <div className="Home">
                 <section className="position-relative mt-5 row g-0 align-items-center">
                     <div className="Social col-md-2">
-                        <a href="https://github.com/Beylax" target="_blank" rel="noreferrer">
+                        <a className="col-4 col-md-12" href="https://github.com/Beylax" target="_blank" rel="noreferrer">
                             <img src={require("../Images/github-logo.png")} alt="https://github.com/Beylax"/>
                         </a>
-                        <a href="https://www.linkedin.com/manuel-baldoni-765342245" target="_blank" rel="noreferrer">
+                        <a className="col-4 col-md-12" href="https://www.linkedin.com/manuel-baldoni-765342245" target="_blank" rel="noreferrer">
                             <img src={require("../Images/linkedin-logo.png")} alt="www.linkedin.com/in/manuel-baldoni-765342245"/>
                         </a>
-                        <a href="https://www.instagram.com/_baldo._/" target="_blank" rel="noreferrer">
+                        <a className="col-4 col-md-12" href="https://www.instagram.com/_baldo._/" target="_blank" rel="noreferrer">
                             <img src={require("../Images/instagram-logo.png")} alt="https://www.instagram.com/_baldo._/"/>
                         </a>
                     </div>
