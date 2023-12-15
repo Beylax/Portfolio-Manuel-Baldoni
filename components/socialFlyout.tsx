3e7e2c6eff5e5d65abf017ef4d4ac7eca2bc5c7a
beylax <manuel.baldoni04@gmail.com>
2023-12-15 11:16:51 +0100

    Added overflow hidden to html when cv is open

--- components/socialFlyout.tsx
+++ components/socialFlyout.tsx
@@ -11,7 +11,10 @@ export default function SocialFlyout() {
     return (
         <>
             <div className="fixed left-4 top-1/2 translate-y-[-50%] hidden lg:flex flex-col gap-4 z-[10000]">
-                <button className="w-[50px] h-[50px] p-2 flex items-center justify-center z-[10000] bg-gradient-to-r from-highlight to-highlight50 rounded-full z-[100000]" onClick={() => setIsVisible(true)}>
+                <button className="w-[50px] h-[50px] p-2 flex items-center justify-center z-[10000] bg-gradient-to-r from-highlight to-highlight50 rounded-full z-[100000]" onClick={() => {
+                    setIsVisible(true)
+                    document.querySelector("html")?.classList?.add("overflow-hidden")
+                }}>
                     <span className="text-sm font-bold ">CV</span>
                 </button>
                 <Link href="https://www.linkedin.com/in/manuel-baldoni" target="__blank" className="w-[50px] h-[50px] p-2 flex items-center justify-center bg-gradient-to-r from-highlight to-highlight50 rounded-full">
@@ -26,7 +29,10 @@ export default function SocialFlyout() {
             </div>
             <div className={`fixed inset-0 bg-[rgba(0,0,0,0.8)] p-6 lg:p-10 transition-all duration-300 z-[1000000] ${!isVisible ? "opacity-0 invisible pointer-events-none" : ""}`}>
                 <iframe src={"/cv.pdf"} className="h-full w-full"></iframe>
-                <button className="absolute top-2 right-2" onClick={() => setIsVisible(false)}>
+                <button className="absolute top-2 right-2" onClick={() => {
+                    setIsVisible(false)
+                    document.querySelector("html")?.classList?.remove("overflow-hidden")
+                }}>
                     <Icon icon="close" fill={tailwindConfig.theme.extend.colors["main"]} />
                 </button>
             </div>
