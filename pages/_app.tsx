228ba6fd581db1833bbd378103451bc3b4d354a7
Manuel Baldoni <manuel.baldoni04@gmail.com>
2024-06-19 09:42:17 +0200

    Added google tag manager, and updated next js version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
+import { GoogleTagManager } from '@next/third-parties/google'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <GoogleTagManager gtmId={"GTM-N9VXK56B"} />
+    </>
   )
 }
 
