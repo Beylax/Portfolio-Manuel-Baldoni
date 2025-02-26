2faa9496bf2d3145cbd6b7b5f6d3a24d08fcc509
Manuel Baldoni <manuel.baldoni04@gmail.com>
2025-02-26 19:26:42 +0100

    Update fiverLink.tsx

--- components/fiverLink.tsx
+++ components/fiverLink.tsx
@@ -4,7 +4,7 @@ export default function FiverLink() {
 	return (
 		<Link
 			href={"https://it.fiverr.com/s/XL0ExYZ"}
-			className={`block w-fit mx-auto mt-10 rainbow-border text-main shadow-[0_0_10px_-2px_#ffffff] px-6 py-3 text-center text-xl font-bold uppercase rounded-md`}
+			className={`fiver-link block w-fit mx-auto mt-10 rainbow-border text-main shadow-[0_0_10px_-2px_#ffffff] px-6 py-3 text-center text-xl font-bold uppercase rounded-md`}
 		>
 			{"If you'd like to work with me check out my fiver! I'll see you there"}
 		</Link>
